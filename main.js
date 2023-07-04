9f930ddc754677288002cedf94982a57a728f2b3
NowWeAre-Free <kontodarm@wp.pl>
2023-07-04 20:16:58 +0200

    add input password validation, display txt depends on what user typed in input, lowered case to each password in list

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-// Projekt 5 - Zadanie dla Ciebie 
+// Projekt 5 - Zadanie dla Ciebie
 // Sprawdź czy wpisane w input hasło pasuje do któegoś z hasel w tablicy. Jeśli tak wyświetl wiadomość w <div>
-//wielkość liter nie ma znaczenia (wykorzystaj metodę .toUpperCase lub .toLowerCase - pamiętaj, że metody te działają na pojedyńczym elemencie typu string)
+//wielkość liter nie ma znaczenia (wykorzystaj metodę .toUpperCase lub .toLowerCase -
+// pamiętaj, że metody te działają na pojedyńczym elemencie typu string)
 
 const input = document.querySelector("input");
-const passwords = ['jedEN', 'DwA'];
-const messages = ["super", "działa!"]
+const passwords = ["jedEN", "DwA"];
+const messages = ["super", "działa!"];
 
+const div = document.querySelector("div");
 const showMessage = (e) => {
- //tutaj rozwiązanie
- // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
-}
+  div.textContent = "";
+  //tutaj rozwiązanie
+  // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie
+  //   ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
+  passwords.forEach((password, i) => {
+    if (password.toLowerCase() === e.target.value) {
+      div.textContent = messages[i];
+      e.target.value = "";
+    }
+  });
+};
 
-input.addEventListener("input", showMessage)
-
-
-
+input.addEventListener("input", showMessage);
